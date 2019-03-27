d607bf82d9875aac5c867b94ee2a50dd2b130318
Jonas Juselius <jonas.juselius@itpartner.no>
2019-03-27 19:34:59 +0100

    Add Entity Framework

---
+++ src/Entity/Context.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Entity.Entity
+{
+    public partial class toodelooContext : DbContext
+    {
+        private string connString = "";
+
+        public toodelooContext()
+        {
+        }
+
+        public toodelooContext( string connectionString ) {
+            this.connString = connectionString;
+        }
+
+        public toodelooContext(DbContextOptions<toodelooContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todo { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(this.connString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.3-servicing-35854");
+
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+            });
+        }
+    }
+}
---
+++ src/Entity/Model/Todo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entity.Entity
+{
+    [Table("todo", Schema = "dbo")]
+    public partial class Todo
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("title")]
+        [StringLength(200)]
+        public string Title { get; set; }
+        [Column("description")]
+        [StringLength(2000)]
+        public string Description { get; set; }
+        [Column("priority")]
+        public int? Priority { get; set; }
+        [Column("due", TypeName = "date")]
+        public DateTime? Due { get; set; }
+    }
+}
