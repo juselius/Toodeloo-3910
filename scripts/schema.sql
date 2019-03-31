20e40faf9a5d90144ff86043a5f17d4386b4d2e2
Jonas Juselius <jonas.juselius@itpartner.no>
2019-03-31 21:35:32 +0200

    I suck at sql

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,12 +1,12 @@
 ﻿create schema dbo;
 
 create table dbo.todo (
-    id int not null,
+    id serial,
     title varchar (200) null,
     description varchar (2000) null,
     priority int null,
     due date null,
-    primary key  (id)
+    primary key (id)
 );
 
 
