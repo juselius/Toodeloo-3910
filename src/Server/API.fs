c3fc50bf1e64de2c58d78c20d2f96e69b4937a72
Jonas Juselius <jonas.juselius@itpartner.no>
2019-04-03 14:01:43 +0200

    Implement remaining db funcs, and move to separate module.

--- src/Server/API.fs
+++ src/Server/API.fs
@@ -5,36 +5,9 @@ open Microsoft.AspNetCore
 open FSharp.Control.Tasks.V2
 open Giraffe
 open Shared
+open DbContext
 
-let mutable private db = Map.ofList [(1, defaultTodo); (2, defaultTodo)]
-
-let addTodo item = 
-    let entry = Entity.Todo()
-    entry.Title <- item.title
-    entry.Description <- item.description
-    entry.Priority <- Nullable item.priority 
-    entry.Due <- Option.toNullable item.due
-    let f (ctx : Entity.ToodelooContext) = 
-        ctx.Add entry |> ignore
-        ctx.SaveChanges () |> ignore
-        entry
-    DbContext.withDb f
-
-let asTodo (t : Entity.Todo) = 
-    {
-        title = t.Title
-        description = t.Description
-        priority = t.Priority.Value
-        due = Option.ofNullable t.Due
-    }
-
-let getTodos () = 
-    let f (ctx : Entity.ToodelooContext) = 
-        query {
-            for t in ctx.Todo do
-                select t
-        } |> Seq.map asTodo
-    DbContext.withDb f
+// let mutable private db = Map.ofList [(1, defaultTodo); (2, defaultTodo)]
 
 let saveEntry next (ctx : Http.HttpContext) =
     task {
--- src/Server/DbContext.fs
+++ src/Server/DbContext.fs
@@ -1,6 +1,8 @@
 module Toodeloo.DbContext
 
+open System
 open Microsoft.EntityFrameworkCore
+open Shared
 
 let inline internal withDb qry =
     try
@@ -16,3 +18,54 @@ let tryMigrate () =
     match withDb (fun ctx -> ctx.Database.Migrate ()) with
     | Ok _ -> printfn "done."
     | Error e -> printfn "exception in Db.tryMigrate: \n%s" (string e)
+
+let addTodo item = 
+    let entry = Entity.Todo()
+    entry.Title <- item.title
+    entry.Description <- item.description
+    entry.Priority <- Nullable item.priority 
+    entry.Due <- Option.toNullable item.due
+    let f (ctx : Entity.ToodelooContext) = 
+        ctx.Add entry |> ignore
+        ctx.SaveChanges () |> ignore
+        entry
+    withDb f
+
+let asTodo (t : Entity.Todo) = 
+    {
+        title = t.Title
+        description = t.Description
+        priority = t.Priority.Value
+        due = Option.ofNullable t.Due
+    }
+
+let getTodos () = 
+    let f (ctx : Entity.ToodelooContext) = 
+        query {
+            for t in ctx.Todo do
+                select t
+        } |> Seq.map asTodo
+    withDb f
+
+
+let getItemById id (ctx : Entity.ToodelooContext) = 
+        query {
+            for t in ctx.Todo do
+            where (t.Id = id)
+            select t
+        } 
+        |> Seq.tryHead
+let getTodo id = 
+    withDb (getItemById id >> Option.map asTodo)
+
+let delTodo id = 
+    withDb (fun ctx -> 
+        getItemById id ctx 
+        |> Option.map (ctx.Remove >> ignore >> ctx.SaveChanges)
+    )
+
+let updateTodo id = 
+    withDb (fun ctx -> 
+        getItemById id ctx 
+        |> Option.map (ctx.Update >> ignore >> ctx.SaveChanges)
+    )
